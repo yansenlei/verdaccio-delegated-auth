fb12a6e49b06fd24ad0ebf8781aca2ff46dd76f2
Cheng.Lee <helloleecheng@gmail.com>
2019-05-09 19:46:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,41 @@ const axios = require('axios')
 
 module.exports = DelegatedAuthentication
 
-const DEFAULT_CACHE = 30
-let ch = {}
-
 function DelegatedAuthentication(config, stuff) {
-  var self = Object.create(DelegatedAuthentication.prototype)
+    var self = Object.create(DelegatedAuthentication.prototype)
 
-  // config for this module
-  self._config = config
-  self._logger = stuff.logger
-  self._logger.warn('Auth plugin configuration:\n', config)
+    // config for this module
+    self._config = config
+    self._logger = stuff.logger
+    self._logger.warn('Auth plugin configuration:\n', config)
 
-  var url = self._config.url
-  if (!url) throw new Error('should specify "url" in config')
+    var url = self._config.url
+    if (!url) throw new Error('should specify "url" in config')
 
-  return self
+    return self
 }
 
 DelegatedAuthentication.prototype.authenticate = function (user, password, cb) {
-  let self = this
-  let params = {}
-  let _user = this._config.email ? `${user}@${this._config.email}` : user
-  params[this._config.user_key || 'username'] = _user
-  params[this._config.pwd_key || 'password'] = password
-  if(ch[user] && ch[user] > new Date().getTime()) {
-    return cb(null, [user])
-  } else {
+    let self = this
+    let params = {}
+    let _user = this._config.email ? `${ user }@${ this._config.email }` : user
+    params[this._config.user_key || 'username'] = _user
+    params[this._config.pwd_key || 'password'] = password
+
     self._logger.warn('Authentication request: ', _user)
     axios.post(self._config.url, params)
-    .then(function (res) {
-      if(res.data.error) { // json-rpc error
-        self._logger.warn('Authentication failed. ', res.data.error.code ?  `code: ${res.data.error.code}` : 'not error code')
-        return cb(null, false)
-      } else { // success: supports API style for RESTful and JSON-rpc
-        self._logger.warn('Authentication succeeded')
-        ch[user] = new Date().getTime() + 1000 * (self._config.cache || DEFAULT_CACHE)
-        return cb(null, [user])
-      }
-    })
-    .catch(function (error) {
-      self._logger.warn('Authentication failed: ', _user)
-      return cb(null, false)
-    });
-  }
+        .then(function (res) {
+            if (res.data.error) { // json-rpc error
+                self._logger.warn('Authentication failed. ', res.data.error.code ? `code: ${ res.data.error.code }` : 'not error code')
+                return cb(null, false)
+            } else { // success: supports API style for RESTful and JSON-rpc
+                self._logger.warn('Authentication succeeded')
+                return cb(null, [user])
+            }
+        })
+        .catch(function (error) {
+            self._logger.warn('Authentication failed: ', _user)
+            return cb(null, false)
+        })
+
 }
